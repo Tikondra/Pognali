c421c13e10cf8614dbe2523d02269cfe9e3b9c37
Tikondra <tikondra47@gmail.com>
2019-11-20 22:49:51 +0400

    mobail menu, logo m and t

---
+++ source/js/script.js
@@ -0,0 +1,24 @@
+function menu () {
+
+  var header = document.querySelector('.page-header');
+  var navMain = document.querySelector('.page-header__info');
+  var navToggle = document.querySelector('.page-header__menu-open');
+
+  header.classList.remove('page-header--no-js');
+
+    navToggle.addEventListener('click', function() {
+
+      navToggle.classList.toggle('page-header__menu-open--close');
+
+      if (navMain.classList.contains('page-header__info--close')) {
+        navMain.classList.remove('page-header__info--close');
+        navMain.classList.add('page-header__info--open');
+      } else {
+        navMain.classList.add('page-header__info--close');
+        navMain.classList.remove('page-header__info--open');
+      }
+    });
+};
+
+
+menu();
