a49250b3c5f6ee0b4d19fb3216ddd05f6e2d9bc9
Tikondra <tikondra47@gmail.com>
2019-11-30 16:29:17 +0400

    процесс

--- source/js/script.js
+++ source/js/script.js
@@ -20,5 +20,25 @@ function menu () {
     });
 };
 
+function countryPopup () {
+
+  var toggle = document.querySelector('.country-popup__open-btn');
+  var popup = document.querySelector('.country-popup__box');
+
+  header.classList.remove('page-header--no-js');
+
+    navToggle.addEventListener('click', function() {
+
+      navToggle.classList.toggle('page-header__menu-open--close');
+
+      if (navMain.classList.contains('page-header__info--close')) {
+        navMain.classList.remove('page-header__info--close');
+        navMain.classList.add('page-header__info--open');
+      } else {
+        navMain.classList.add('page-header__info--close');
+        navMain.classList.remove('page-header__info--open');
+      }
+    });
+};
 
 menu();
