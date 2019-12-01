26837779b3e55486f68fb5d3f7e3129e7d0aba33
Tikondra <tikondra47@gmail.com>
2019-12-02 00:51:56 +0400

    header, menu-scroll

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,9 @@
 function menu () {
 
   var header = document.querySelector('.page-header');
-  var navMain = document.querySelector('.page-header__info');
-  var navToggle = document.querySelector('.page-header__menu-open');
-  var navWrap = document.querySelector('.wrapper-header');
+  var navMain = header.querySelector('.page-header__info');
+  var navToggle = header.querySelector('.page-header__menu-open');
+  var logo = header.querySelector('.page-header__logo');
 
   header.classList.remove('page-header--no-js');
 
@@ -14,20 +14,27 @@ function menu () {
       if (navMain.classList.contains('page-header__info--close')) {
         navMain.classList.remove('page-header__info--close');
         navMain.classList.add('page-header__info--open');
+        logo.classList.add('page-header__logo--blue');
       } else {
         navMain.classList.add('page-header__info--close');
         navMain.classList.remove('page-header__info--open');
+        logo.classList.remove('page-header__logo--blue');
       }
     });
 
     window.addEventListener("scroll", function(){
-      navWrap.classList.add("wrapper-header--scroll");
-      if(pageYOffset === 0) {
-        navWrap.classList.remove("wrapper-header--scroll");
+
+      if(navMain.classList.contains("page-header__info--close")) {
+        header.classList.add("page-header--scroll");
+        logo.classList.add('page-header__logo--blue');
+        navToggle.classList.add('page-header__menu-open--scroll');
+      }
+      if(pageYOffset === 0 && navMain.classList.contains("page-header__info--close")) {
+        header.classList.remove("page-header--scroll");
+        logo.classList.remove('page-header__logo--blue');
+        navToggle.classList.remove('page-header__menu-open--scroll');
       }
     });
 };
-
-
 
 menu();
